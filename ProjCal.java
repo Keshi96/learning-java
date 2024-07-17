e918f9f85991bd5746bbb31e1857816ef905857e
Keshi96 <keshijoeyes@gmail.com>
2024-07-17 11:36:53 +0100

    Rename some class files

---
+++ ProjCal.java
@@ -0,0 +1,9 @@
+public class Keshi{
+    public static void main(String[]Args){
+        short b = 4;
+        short c = 6;
+// fghhggh
+        System.out.println(b+c);
+    
+    }
+}
--- inheritance.java
+++
@@ -1,9 +0,0 @@
-public class Keshi{
-    public static void main(String[]Args){
-        short b = 4;
-        short c = 6;
-// fghhggh
-        System.out.println(b+c);
-    
-    }
-}
